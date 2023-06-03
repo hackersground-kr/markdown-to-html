b65ccb2e24a798119310258cc023239abaf59619
Justin Yoo <justin.yoojh@gmail.com>
2023-06-03 17:05:01 +0900

    Restructure the existing app

---
+++ src/MD2Html.FunctionApp/Startup.cs
@@ -0,0 +1,52 @@
+using System;
+
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Abstractions;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Configurations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+[assembly: FunctionsStartup(typeof(MD2Html.FunctionApp.Startup))]
+
+namespace MD2Html.FunctionApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            builder.ConfigurationBuilder
+                   .AddEnvironmentVariables();
+
+            base.ConfigureAppConfiguration(builder);
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            ConfigureAppSettings(builder.Services);
+        }
+
+        private static void ConfigureAppSettings(IServiceCollection services)
+        {
+            var options = new DefaultOpenApiConfigurationOptions()
+            {
+                OpenApiVersion = OpenApiVersionType.V3,
+                Info = new OpenApiInfo()
+                {
+                    Version = "1.0.0",
+                    Title = "Markdown to HTML converter API",
+                    Description = "Convert a markdown document to the HTML one",
+                },
+            };
+            /* ⬇️⬇️⬇️ for GH Codespaces ⬇️⬇️⬇️ */
+            var codespaces = bool.TryParse(Environment.GetEnvironmentVariable("OpenApi__RunOnCodespaces"), out var isCodespaces) && isCodespaces;
+            if (codespaces)
+            {
+                options.IncludeRequestingHostName = false;
+            }
+            /* ⬆️⬆️⬆️ for GH Codespaces ⬆️⬆️⬆️ */
+            services.AddSingleton<IOpenApiConfigurationOptions>(options);
+        }
+    }
+}
